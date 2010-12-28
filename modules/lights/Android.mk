4fa16d582d44282f31ef71c84d80657ab9fcae02
guoyin.chen <guoyin.chen@freescale.com>
2010-12-29 07:20:47 +0800

    ENGR00137445 Update Android.mk to add LOCAL_MODULE_TAGS := optional
    Update Android.mk to add LOCAL_MODULE_TAGS := optional
    Signed-off-by: guoyin.chen <guoyin.chen@freescale.com>

--- modules/lights/Android.mk
+++ modules/lights/Android.mk
@@ -22,4 +22,5 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_SRC_FILES := fsl_lights.c
 LOCAL_MODULE := lights.freescale
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
